611c2ce7582cdd714e66b54a55caa06bc17bef5e
h-wal <harsh2102agarwal@gmail.com>
2025-07-24 22:28:08 +0530

    created auth routes

--- excali/apps/http-backend/src/index.ts
+++ excali/apps/http-backend/src/index.ts
@@ -2,4 +2,12 @@ import express from "express"
 
 const app = express();
 const PORT = 3008;
+
+app.post("/signup", signUpRouter)
+app.post("/signin", signInRouter)
+app.post("/room", roomRouter)
+
+
+
+
 app.listen(PORT)
