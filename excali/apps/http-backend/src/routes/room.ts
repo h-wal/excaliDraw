6333ff15ab78ac589b43a84b16b6b56e8b6b7557
h-wal <harsh2102agarwal@gmail.com>
2025-08-08 12:57:38 +0530

    added chat route in http be

--- excali/apps/http-backend/src/routes/room.ts
+++ excali/apps/http-backend/src/routes/room.ts
@@ -8,40 +8,23 @@ const {prismaClient} = db;
 
 const roomRouter: Router = express.Router();
 
-async function createroomRouterfunction(req: Request, res: Response){
-    const authHeader = req.headers['authorization'];
-    const id = req.query.id as string;
+async function getChatsRouterfunction(req: Request, res: Response){
+    const roomId = Number(req.params.roomId);
+    const message = await prismaClient.room.findMany({
+        where:{
+            id: roomId
+        },
+        orderBy:{
+            id: "desc"
+        },
+        take: 50
+    });
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        return res.status(401).json({ message: "Missing or invalid token" });
-    }
-
-    const token = authHeader.split(" ")[1] as string
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET as string)
-
-    if (typeof decoded == "object"){
-        const room = await prismaClient.room.create({
-            data:{
-                slug : id,
-                admin: {
-                    connect: {id: decoded.userid}
-                }
-            }
-        })
-        return res.status(201).json({ room })
-    } 
-    else{
-        return res.status(401).json({ message: "Invalid token" });
-    }
+    res.json({
+        message: message
+    })
 }
 
-async function joinroomRouterfunction(req: Request, res: Response){
-
-}
-
-
-roomRouter.post("/create", createroomRouterfunction)
-roomRouter.post("/join", joinroomRouterfunction)
+roomRouter.get("/chats/:roodId", getChatsRouterfunction)
 
 export default roomRouter
