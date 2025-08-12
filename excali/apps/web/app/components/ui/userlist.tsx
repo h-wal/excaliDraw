0c137870e5b7d1f6951333574ba0880c47f85336
h-wal <harsh2102agarwal@gmail.com>
2025-08-12 21:59:15 +0530

    structuring search bar

---
+++ excali/apps/web/app/components/ui/userlist.tsx
@@ -0,0 +1,16 @@
+import SearchIcon from "../icons/searchIcon"
+import SearchBar from "./searchbar"
+
+export default function UserList(){
+    return(
+        <div className="bg-white basis-4/12 rounded-3xl m-4 shadow-2xl flex p-4 flex-col">
+            <div className="text-black text-2xl font-sans text-gray-800 font-bold my-4">
+                Chat
+            </div>
+            <SearchBar />
+            <div>
+                
+            </div>
+        </div>
+    )
+} 
