eaeab934221b8f477dd2a53e10cb5999d0e7da6f
h-wal <harsh2102agarwal@gmail.com>
2025-08-14 16:37:30 +0530

    chat heading added

--- excali/apps/web/app/components/ui/chatbox.tsx
+++ excali/apps/web/app/components/ui/chatbox.tsx
@@ -10,6 +10,7 @@ interface userProp{
 
 
 export default function ChatBox(prop: userProp){
+    console.log("hi" +prop.user)
     return(
         <div className="basis-8/12 rounded-3xl m-2 shadow-xl flex flex-col">
             <div className="basis-11/12 flex flex-col">
@@ -18,7 +19,7 @@ export default function ChatBox(prop: userProp){
                         <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
                     </div>}
                     <div className="text-black flex items-center">
-                        { (prop.user) ? prop.user.uname : 'Kindly Select a User to Chat'}
+                        { (prop.user) ? JSON.stringify(prop.user): 'Kindly Select a User to Chat'}
                     </div>
                 </div>
                 <div id="window" className="basis-6/7">
